34f07ff5caee2ab08f21022920f2577c437a246d
William Fish <william.michael.fish@gmail.com>
2026-06-03 08:44:57 +0100

    chore(release): release v0.5.0 (v0.4 TUI polish milestone) (#64)

--- flake.nix
+++ flake.nix
@@ -103,7 +103,7 @@
 
         wallsPkg = rustPlatform.buildRustPackage {
           pname = "walls";
-          version = "0.4.0";
+          version = "0.5.0";
           src = wallsSrc;
           cargoLock.lockFile = ./Cargo.lock;
           cargoBuildFlags = [
