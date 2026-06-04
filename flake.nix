355f2e27985f73f59db25fbc6a82b1584604fcc3
William Fish <william.michael.fish@gmail.com>
2026-06-04 18:27:43 +0100

    BAU: Release v0.6.2

--- flake.nix
+++ flake.nix
@@ -103,7 +103,7 @@
 
         wallsPkg = rustPlatform.buildRustPackage {
           pname = "walls";
-          version = "0.6.1";
+          version = "0.6.2";
           src = wallsSrc;
           cargoLock.lockFile = ./Cargo.lock;
           cargoBuildFlags = [
